8b947891c9c791774497c04fe82654b905537722
Nurlibay <koshkinbayevn@gmail.com>
2022-11-15 14:46:37 +0500

    initial commit

---
+++ app/src/main/java/uz/nurlibaydev/moneymanager/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package uz.nurlibaydev.moneymanager.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.nurlibaydev.moneymanager.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/uz/nurlibaydev/moneymanager/utils/extenions/ViewGroupExtensions.kt
@@ -0,0 +1,12 @@
+package uz.nurlibaydev.moneymanager.utils.extenions
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.viewbinding.ViewBinding
+
+
+fun Dialog.config(viewBinding: ViewBinding) {
+    window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    setContentView(viewBinding.root)
+}
