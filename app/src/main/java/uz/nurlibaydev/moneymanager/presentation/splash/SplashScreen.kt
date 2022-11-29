65357f86a4a70961e3c2f579d561760a94318a1a
Nurlibay <koshkinbayevn@gmail.com>
2022-11-29 21:19:42 +0500

    some errors fixed!

--- app/src/main/java/uz/nurlibaydev/moneymanager/presentation/splash/SplashScreen.kt
+++ app/src/main/java/uz/nurlibaydev/moneymanager/presentation/splash/SplashScreen.kt
@@ -20,7 +20,7 @@ class SplashScreen : Fragment(R.layout.screen_splash) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        lifecycleScope.launch {
+        lifecycleScope.launchWhenResumed {
             delay(1500)
             if (pref.isSigned) {
                 navController.navigate(SplashScreenDirections.actionSplashScreenToMainContainer())
